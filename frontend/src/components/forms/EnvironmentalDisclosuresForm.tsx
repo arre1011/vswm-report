e38d9409679d6780c7f61e6daada80abb343362f
Rene <r.m.arnegger@googlemail.com>
2025-11-29 09:30:14 +0700

    update inhalt of wizard elements

--- frontend/src/components/forms/EnvironmentalDisclosuresForm.tsx
+++ frontend/src/components/forms/EnvironmentalDisclosuresForm.tsx
@@ -1,6 +1,16 @@
 
-export function EnvironmentalDisclosuresForm() {
+import { useWizard } from "@/stores/WizardStore"
+import { ChangeEvent } from "react"
 
+
+export function EnvironmentalDisclosuresForm() {
+  const { data, updateEnvironmentalDisclosures } = useWizard()
+  const environmental = data.environmentalDisclosures
+
+  const handleInputChange =
+    (field: keyof typeof environmental) => (event: ChangeEvent<HTMLInputElement>) => {
+      updateEnvironmentalDisclosures({ [field]: event.target.value })
+    }
 
   return (
     <>
--- frontend/src/components/forms/GovernanceDisclosuresForm.tsx
+++ frontend/src/components/forms/GovernanceDisclosuresForm.tsx
@@ -1,6 +1,16 @@
 
-export function GovernanceDisclosuresForm() {
+import { useWizard } from "@/stores/WizardStore"
+import { ChangeEvent } from "react"
 
+
+export function GovernanceDisclosuresForm() {
+  const { data, updateGovernanceDisclosures } = useWizard()
+  const governance = data.governanceDisclosures
+
+  const handleChange =
+    (field: keyof typeof governance) => (event: ChangeEvent<HTMLInputElement>) => {
+      updateGovernanceDisclosures({ [field]: event.target.value })
+    }
 
   return (
     <>
